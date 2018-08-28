64b5e3def4e64f12c40522c6e7c56a11d707d8db
jamie reaves <jamiereaves@utexas.edu>
2018-08-27 23:22:17 -0500

    again

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const keys = require("../keys");
+console.log("HEY API HEROKU CONFIG VAR" + REACT_APP_NYT_KEY)
 
 export default {
   // Returns articles from the NYT API
