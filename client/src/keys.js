218040191d50c03784773596d0ffa619f154251b
jamie reaves <jamiereaves@utexas.edu>
2018-08-27 23:37:39 -0500

    again.this api key security is kicking my ass

--- client/src/keys.js
+++ client/src/keys.js
@@ -1,5 +1,6 @@
 console.log("keys.js loaded correctly");
 console.log(process.env.REACT_APP_NYT_KEY);
+console.log("CONFIG: " + process.env.NYT_KEY);
 
 exports.nyt = {
   key: process.env.REACT_APP_NYT_KEY
